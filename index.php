2d1be184bc3d8e8a85570db03661d4e184d1ad52
Lucas Maieski <lucasgrigol@gmail.com>
2022-12-28 18:48:03 -0300

    commit inicial

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php get_header(); ?>
+
+<section>
+    <div class="container conteudo">
+        <div class="row">
+            <div class="col-md-8 maincontent">
+                <?php if(have_posts()): ?>
+                    <?php while(have_posts()): ?>
+                         <?php the_post(); ?>
+                         <?php get_template_part('template_parts/post'); ?>
+                    <?php endwhile; ?>
+                    <div class="pag">
+                        <div class="previous_pag"> <?php previous_posts_link('Página Anterior'); ?> </div>
+                        <div class="next_pag"> <?php next_posts_link('Próxima Página'); ?> </div>
+                    </div>
+                <?php endif; ?>
+            </div>
+            <?php get_sidebar(); ?>
+        </div>
+    </div>
+</section>
+
+<?php get_footer(); ?>
