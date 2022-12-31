90d0303ec7bbf65973bfd903b6657a94dd820f6a
Lucas Maieski <lucasgrigol@gmail.com>
2022-12-31 15:41:24 -0300

    adicionando Widgets ao tema

---
+++ sidebar.php
@@ -0,0 +1,9 @@
+<div class="col-md-4 sidebar">
+    <aside>
+        <?php 
+        if(is_active_sidebar('lm_sidebar')){
+            dynamic_sidebar('lm_sidebar');
+        }
+        ?>
+    </aside>
+</div>
